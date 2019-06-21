3f1c0d7a3008b0470cbd364a15899ddcfb116baf
pc <eugenezaharchenko@gmail.com>
2019-06-21 21:23:23 +0300

    requirements

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_moment import Moment
+# from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from .api import api_blueprint, api
 
-moment = Moment()
+# moment = Moment()
 db = SQLAlchemy()
 
 
@@ -12,7 +12,8 @@ def create_app():
     app = Flask(__name__)
     api.init_app(app)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://arthur:arthur234@localhost/projects'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://e:password@localhost/flask_api'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.register_blueprint(api_blueprint)
     with app.app_context():
         db.init_app(app)
